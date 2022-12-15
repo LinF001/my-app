65eeed1189946fd0793665712f750a4eec325144
Fangyu Lin <linf@aetna.com>
2022-12-15 13:26:33 -0500

    update text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Auto-deploy the React
         </a>
       </header>
     </div>
